81c3dabe93cd02bf8070cbbea0fbf0d54207db4a
wglambert <W.lambert1031@gmail.com>
2018-06-05 11:00:31 -0700

    Update rcat.sh

--- rcat.sh
+++ rcat.sh
@@ -1,38 +1,59 @@
 #!/bin/bash
+
 echo -e ""
-echo "directory to recursively search, default is ./"
+echo "Directory to recursively search, default is ./"
  read -e dir
  echo -e""
   if [ -z "$dir" ]; then \
    dir="./"
   fi
-echo "filename to search, defaults to every file"
+echo "Filename to search, defaults to every file"
  read -e file
  echo -e ""
   if [ -z "$file" ]; then \
    file=""
   fi
-echo "text to search for, default is output everything"
+echo "Text to search for, default is output everything"
  read -e text
  echo -e ""
-  if [ -z $"text" ]; then \
+  if [ -z "$text" ]; then \
    text=""
   fi
 
 echo -e "------------SEARCHING------------\n"
-if [ -z "$file" -a -z "$text" ]; then \
- tail -n +1 $(find "$dir" -not -type d)
-        echo -e "------------INPUT------------\ntail -n +1 $(find "$dir" -not -type d)\n-----------------------------\n"
+if [ -z "$file" -a -z "$text" ]; then
+        for i in $(find "$dir" -not -type d);
+        do
+                echo -e "==>\033[0;36m "$i" \033[0m<=="
+                cat "$i"
+                printf "\n"
+        done
+echo -e "------------INPUT------------\ncat \$(find "$dir" -not -type d)\n"
 
-elif [ -z "$fil"e -a -n "$text" ]; then \
- tail -n +1 $(find "$dir" -not -type d) | grep -C 1 "$text"
-        echo -e "------------INPUT------------\ntail -n +1 $(find "$dir" -not -type d) | grep -C 1 "$text"\n-----------------------------\n"
+elif [ -z "$file" -a -n "$text" ]; then 
+        for i in $(grep -il "$text" $(find "$dir" -not -type d));
+        do
+                echo -e "==>\033[0;36m "$i" \033[0m<=="
+                grep --color=auto -iC 1 "$text" "$i"
+                printf "\n"
+        done 
+echo -e "------------INPUT------------\ngrep -iC 1 "$text" \$(find "$dir" -not -type d)\n"
 
-elif [ -n "$file" -a -n "$text" ]; then \
- tail -n +1 $(find "$dir" -name "$file" -not -type d) | grep -C 1 "$text"
-        echo -e "------------INPUT------------\ntail -n +1 $(find "$dir" -name "$file" -not -type d) | grep -C 1 "$text"\n-----------------------------\n"
+elif [ -n "$file" -a -n "$text" ]; then 
+        for i in $(grep -il "$text" $(find "$dir" -name "$file" -not -type d));
+        do
+                echo -e "==>\033[0;36m "$i" \033[0m<=="
+                grep --color=auto -iC 1 "$text" "$i"
+                printf "\n"
+        done
+echo -e "------------INPUT------------\ngrep -iC 1 "$text" \$(find "$dir" -name "$file" -not -type d)\n"
 
-elif [ -n "$file" -a -z "$text" ]; then \
- tail -n +1 $(find "$dir" -name "$file" -not -type d)
-        echo -e "------------INPUT------------\ntail -n +1 $(find "$dir" -name "$file" -not -type d)\n-----------------------------\n"
+elif [ -n "$file" -a -z "$text" ]; then
+        for i in $(find "$dir" -name "$file" -not -type d);
+        do
+                echo -e "==>\033[0;36m "$i" \033[0m<=="
+                cat "$i"
+                printf "\n"
+        done
+echo -e "------------INPUT------------\ncat \$(find "$dir" -name "$file" -not -type d)\n"
 fi
